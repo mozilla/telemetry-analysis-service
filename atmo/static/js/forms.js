45399b5b85d25675430462aa4239d0bd4d1e6009
Jannis Leidel <jannis@leidel.info>
2016-10-13 17:26:08 +0200

    Use 24h based datepicket and prevent dates in the past

--- atmo/static/js/forms.js
+++ atmo/static/js/forms.js
@@ -3,7 +3,12 @@ $(function() {
   $('.datetimepicker').datetimepicker({
     sideBySide: true, // show the time picker and date picker at the same time
     useCurrent: false, // don't automatically set the date when opening the dialog
-    widgetPositioning: {vertical: 'bottom'}, // make sure the picker shows up below the control
-    format: 'YYYY-MM-DD h:mm',
+    format: 'YYYY-MM-DD HH:mm',
+    stepping: 5,
+    minDate: moment().startOf('day'),
+    toolbarPlacement: 'top',
+    showTodayButton: true,
+    showClear: true,
+    showClose: true
   });
 });
