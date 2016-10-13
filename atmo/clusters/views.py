190793af1b9735ae43da9aa4bbef9a8a7646dfe0
Jannis Leidel <jannis@leidel.info>
2016-10-13 16:39:39 +0200

    Add MPL statement to new files

--- atmo/clusters/views.py
+++ atmo/clusters/views.py
@@ -1,3 +1,6 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, you can obtain one at http://mozilla.org/MPL/2.0/.
 from django import forms
 from django.shortcuts import redirect, get_object_or_404, render
 from django.contrib.auth.decorators import login_required
