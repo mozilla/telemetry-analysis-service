62407de7832efc8a0d01497ac74a8be6d073cc6c
Jannis Leidel <jannis@leidel.info>
2016-10-14 13:39:37 +0200

    Fix bug 1309543 - Add filters for clusters in dashboard

--- atmo/views.py
+++ atmo/views.py
@@ -2,10 +2,8 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, you can obtain one at http://mozilla.org/MPL/2.0/.
 import logging
-from datetime import timedelta
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-from django.utils import timezone
 
 from .clusters.models import Cluster
 from .jobs.models import SparkJob
@@ -16,12 +14,26 @@ logger = logging.getLogger("django")
 
 @login_required
 def dashboard(request):
-    clusters = (Cluster.objects.filter(created_by=request.user)
-                               .filter(end_date__gt=timezone.now() - timedelta(days=1))
-                               .order_by("-start_date"))
+    clusters = Cluster.objects.filter(
+        created_by=request.user
+    ).order_by("-start_date")
+
+    # the cluster filter defaults to active ones
+    clusters_filter = request.GET.get('clusters', 'active')
+
+    if clusters_filter == 'active':
+        clusters = clusters.exclude(
+            most_recent_status__in=Cluster.FINAL_STATUS_LIST,
+        )
+    elif clusters_filter == 'inactive':
+        clusters = clusters.filter(
+            most_recent_status__in=Cluster.FINAL_STATUS_LIST,
+        )
+
     jobs = SparkJob.objects.filter(created_by=request.user).order_by("start_date")
     context = {
-        'active_clusters': clusters,
+        'clusters': clusters,
+        'clusters_filter': clusters_filter,
         'user_spark_jobs': jobs,
     }
     return render(request, 'atmo/dashboard.html', context=context)
