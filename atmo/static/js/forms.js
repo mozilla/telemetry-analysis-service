673dbf9b9ddb7e66aa371fdb14cd4878f1d35b42
Jannis Leidel <jannis@leidel.info>
2016-10-17 10:58:09 +0200

    Add JS to disable for submission on submit
    This prevent double submission and adds a reset button, too

--- atmo/static/js/forms.js
+++ atmo/static/js/forms.js
@@ -10,4 +10,32 @@ $(function() {
     showClear: true,
     showClose: true
   });
+
+  $('form').on('submit', function(event){
+    var $form = $(this);
+    var $submit = $form.find('button[type=submit]');
+    var $cancel = $form.find("a:contains('Cancel')");
+    var $reset = $form.find('button[type=reset]');
+    var submit_label = $submit.text();
+    var wait_label = 'Please wait…';
+
+    // disable submit button and change label
+    $submit.addClass('disabled').find('.submit-button').text(wait_label);
+
+    // hide cancel button
+    $cancel.addClass('hidden');
+
+    var reset_callback = function(event) {
+      // re-enable submit button
+      $submit.removeClass('disabled')
+        .find('.submit-button')
+        .text(submit_label);
+      // show cancel button again
+      $cancel.removeClass('hidden');
+      // hide reset button
+      $reset.addClass('hidden');
+    };
+    // show reset button to be able to reset form
+    $reset.removeClass('hidden').click(reset_callback);
+  });
 });
