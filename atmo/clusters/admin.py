de766dedc3e0b2098dce3d63bf4f2fd2f31bb741
Mauro Doglio <mdoglio@mozilla.com>
2016-10-20 19:23:34 +0100

    Bug 1311760 - Add custom admin site for Cluster and SparkJob

--- atmo/clusters/admin.py
+++ atmo/clusters/admin.py
@@ -5,6 +5,29 @@ from django.contrib import admin
 from .models import Cluster
 
 
+def terminate(modeladmin, request, queryset):
+    for cluster in queryset:
+        cluster.terminate()
+
+
 @admin.register(Cluster)
 class ClusterAdmin(admin.ModelAdmin):
-    pass
+    list_display = [
+        'identifier',
+        'size',
+        'created_by',
+        'start_date',
+        'end_date',
+        'jobflow_id',
+        'emr_release',
+        'most_recent_status',
+    ]
+    list_filter = [
+        'most_recent_status',
+        'size',
+        'emr_release',
+        'start_date',
+        'end_date',
+    ]
+    search_fields = ['identifier', 'jobflow_id', 'created_by__email']
+    actions = [terminate]
--- atmo/jobs/admin.py
+++ atmo/jobs/admin.py
@@ -3,6 +3,36 @@ from django.contrib import admin
 from .models import SparkJob
 
 
+def run_now(modeladmin, request, queryset):
+    for job in queryset:
+        job.run()
+
+
 @admin.register(SparkJob)
 class SparkJobAdmin(admin.ModelAdmin):
-    pass
+    list_display = [
+        'identifier',
+        'size',
+        'created_by',
+        'start_date',
+        'end_date',
+        'last_run_date',
+        'is_enabled',
+        'most_recent_status',
+    ]
+    list_filter = [
+        'most_recent_status',
+        'size',
+        'is_enabled',
+        'start_date',
+        'end_date',
+        'last_run_date',
+        'interval_in_hours',
+    ]
+    search_fields = [
+        'identifier',
+        'current_run_jobflow_id',
+        'created_by__email',
+        'most_recent_status',
+    ]
+    actions = [run_now]
