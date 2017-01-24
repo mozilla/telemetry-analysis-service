39da7b8fff5420187c1c3f388cdb744cbde38e7d
Jannis Leidel <jannis@leidel.info>
2017-01-24 15:22:31 +0100

    Add test to fail if there are migrations missing.

---
+++ tests/test_database.py
@@ -0,0 +1,16 @@
+from StringIO import StringIO
+from django.core.management import call_command
+import pytest
+
+
+def test_for_missing_migrations():
+    output = StringIO()
+    try:
+        call_command(
+            'makemigrations', interactive=False, dry_run=True, exit_code=True,
+            stdout=output)
+    except SystemExit as e:
+        # The exit code will be 1 when there are no missing migrations
+        assert unicode(e) == '1'
+    else:
+        pytest.fail("There are missing migrations:\n %s" % output.getvalue())
