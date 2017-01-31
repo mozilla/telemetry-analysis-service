74e0b8d233d91aa5e5ed348b60db892d5456da19
Jannis Leidel <jannis@leidel.info>
2017-01-31 20:23:41 +0100

    Bug 1321358 - Load tabs on reload via location hash. (#172)
    * Bug 1321358 - Load tabs on reload via location hash.
    
    * Bug 1321358 - Minor rename of tooltip JS.

--- atmo/static/js/base.js
+++ atmo/static/js/base.js
@@ -1,12 +1,39 @@
 $(function() {
   AtmoCallbacks = $.Callbacks();
-  var tooltip = function() {
+
+  // load Bootstrap tooltips bubbles
+  var atmoTooltips = function() {
     $('[data-toggle="tooltip"]').tooltip();
+  };
+
+  // load Bootstrap confirmation
+  var atmoConfirmations = function() {
     $('[data-toggle="confirmation"]').confirmation({
       rootSelector: '[data-toggle="confirmation"]',
     });
   };
-  AtmoCallbacks.add(tooltip);
+
+  // jump to tab if tab name is found in document location
+  var atmoTabs = function() {
+    var url = document.location.toString();
+    // show tab matching the document hash
+    if (url.match('#')) {
+      $('.nav-tabs a[href="#' + url.split('#')[1] + '"]').tab('show');
+    }
+
+    // prevent scrolling
+    $('a[data-toggle="tab"]').on('shown.bs.tab', function(event) {
+      if (history.pushState) {
+          history.pushState(null, null, event.target.hash);
+      } else {
+          window.location.hash = event.target.hash; //Polyfill for old browsers
+      }
+    });
+  };
+
+  AtmoCallbacks.add(atmoTooltips);
+  AtmoCallbacks.add(atmoConfirmations);
+  AtmoCallbacks.add(atmoTabs);
   $(document).ready(function() {
     AtmoCallbacks.fire();
   });
