8a3a6a66de8a0c3fcb3ff7a479d975ea91f87aae
Mauro Doglio <mauro.doglio@gmail.com>
2017-03-15 12:31:55 +0000

    Update python version reference in setup.py classifiers (#312)

--- setup.py
+++ setup.py
@@ -17,8 +17,8 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Scientific/Engineering :: Information Analysis'
     ],
