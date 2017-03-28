fac0c0da7b3c7e3609e7c4a741d6a3d9a89a72f0
William Lachance <wlachance@mozilla.com>
2017-03-28 10:33:17 -0400

    Improve formatting of error message when user has no ssh keys

--- atmo/clusters/views.py
+++ atmo/clusters/views.py
@@ -27,8 +27,8 @@ def new_cluster(request):
             request,
             mark_safe(
                 '<h4>No SSH keys associated to you.</h4>'
-                'Please upload one below to be able to launch a cluster.'
-                'This is one-time step.'
+                'Please upload one below to be able to launch a cluster. '
+                'This is a one-time step.'
             )
         )
         return redirect('keys-new')
