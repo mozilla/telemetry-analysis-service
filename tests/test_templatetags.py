9c0b9a84c7e9204e3aebc9a54f8dd15129edac96
Jannis Leidel <jannis@leidel.info>
2017-05-17 14:24:43 +0200

    Add test for markdown templatetag.

--- tests/test_templatetags.py
+++ tests/test_templatetags.py
@@ -4,7 +4,7 @@
 from atmo.clusters.models import Cluster
 from atmo.jobs.templatetags.notebook import is_notebook
 from atmo.jobs.templatetags.status import status_color, status_icon
-from atmo.templatetags import full_url, url_update
+from atmo.templatetags import full_url, markdown, url_update
 
 
 def test_is_notebook():
@@ -39,3 +39,7 @@ def test_status_color():
         assert status_color(status) is None
     for status in Cluster.FAILED_STATUS_LIST:
         assert status_color(status) == 'status-errors'
+
+
+def test_markdown():
+    assert markdown('**test**') == '<p><strong>test</strong></p>'
