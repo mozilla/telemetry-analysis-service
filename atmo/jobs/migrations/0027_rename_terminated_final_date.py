d1275b71afde10d52ab4c1261b6703a1d6b200ef
Jannis Leidel <jannis@leidel.info>
2017-05-23 15:26:12 +0200

    Fix multiple leaf nodes in migration graph.

--- atmo/jobs/migrations/0027_rename_terminated_final_date.py
+++ atmo/jobs/migrations/0027_rename_terminated_final_date.py
@@ -8,7 +8,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('jobs', '0025_populate_job_schedule'),
+        ('jobs', '0026_sparkjobrun_size'),
     ]
 
     operations = [
